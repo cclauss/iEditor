91e7f4eb959556794d7b90af44e0ea8fcd4bc2ed
cclauss <cclauss@bluewin.ch>
2018-08-18 11:01:30 +0200

    dict lookup of firstline based on file extension
    Only ask the user for the firstline if filename has an unrecognized extension.

--- ieditor.py
+++ ieditor.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python3
-import editor, console
+
+import console
+import editor
+
 def main():
-	
-  
 	name = console.input_alert("Name", 'Choose a name for your new file with an extension (e.g. .txt | .html | .py | .md)')
-	firstline = console.input_alert("First line", "Type the first line of your program. Examples:\nPython2: #!/usr/bin/env python2\nPython3: #!/usr/bin/env python3\nNote: HTML, CSS, MarkDown and TxT files are pre-loaded with a simple template.\n----------\niEditor for Pythonista by GoDzM4TT3O.")
-	if '.md' in name:
-		firstline = "# " + name + "\n\n"
-	elif '.html' in name:
-		firstline = "<!--iEditor by GoDzM4TT3O / iGoDz.pw / GoDzM4TT3O.js.org-->\n<!DOCTYPE html>\n<html>\n<head>\n<title>Site title</title>\n</head>\n<body>\n<p>iEditor for Pythonista by <a href=\"https://godzm4tt3o.js.org\">GoDzM4TT3O</a>. <a href=\"https://iGoDz.pw\">Website</a>.</p>\n</body>\n</html>"
-	elif '.txt' in name:
-		firstline = name + "\n" + ("-" * 10) + "\n\n"
-	elif '.css' in name:
-		firstline = "/* iEditor for Pythonista by GoDzM4TT3O */\nhead, body {\n	background-color: blue;\n}"
+	firstline = {
+		'md': "# " + name + "\n\n",
+		'html': ("<!--iEditor by GoDzM4TT3O / iGoDz.pw / GoDzM4TT3O.js.org-->\n"
+			 "<!DOCTYPE html>\n<html>\n<head>\n<title>Site title</title>\n</head>\n<body>\n"
+			 "<p>iEditor for Pythonista by <a href=\"https://godzm4tt3o.js.org\">GoDzM4TT3O</a>. "
+			 "<a href=\"https://iGoDz.pw\">Website</a>.</p>\n</body>\n</html>",
+		'txt': name + "\n" + ("-" * 10) + "\n\n",
+		'css': "/* iEditor for Pythonista by GoDzM4TT3O */\nhead, body {\n	background-color: blue;\n}"
+	}.get(name.split('.')[-1].lower(),
+	      console.input_alert("First line",
+				  ("Type the first line of your program. Examples:\nPython2: #!/usr/bin/env python2\n"
+				   "Python3: #!/usr/bin/env python3\nNote: HTML, CSS, MarkDown and TxT files are "
+				   "pre-loaded with a simple template.\n----------\niEditor for Pythonista by GoDzM4TT3O."))
 	editor.make_new_file(name, firstline)
 	
   
